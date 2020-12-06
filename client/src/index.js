62da29fd04c81e9c7fabc438d3860fe2df0f754f
Matija077 <matija.prs@gmail.com>
2020-12-06 15:48:46 +0100

    add server file, import react and reactDOM

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-console.log("working");
+ReactDOM.render(
+    <main>HEllo there</main>,
+    document.getElementById("root")
+);
