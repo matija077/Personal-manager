4cb4ad3b3a0db1f89f002daafa94421601c66a45
Matija077 <matija.prs@gmail.com>
2020-12-10 20:50:24 +0100

    add App react component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+
+    return(
+        <main className="main">
+            Hello There
+        </main>
+    );
+}
+
+export default App;
