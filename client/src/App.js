a076b185bd87cc8512f8004cdef004ea91095726
Matija077 <matija.prs@gmail.com>
2020-12-14 13:36:06 +0100

    refactor main eclosign tags. add header component

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 import ErrorBoundary from './components/error-boundary/errorBoundary';
 import HomePage from './pages/home/home.page';
+import Header from './components/header/header';
 
 import GlobalStyles from './global.styles';
-import { MainStyles } from './app.styles';
 
 function App() {
 
     return(
         <ErrorBoundary>
-            <MainStyles>
+            <div>
                 <GlobalStyles />
+                <Header>
+
+                </Header>
                 <HomePage>
                 </HomePage>
-            </MainStyles>
+            </div>
         </ErrorBoundary>
     );
 }
---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {} from './header.styles';
+
+function Header() {
+
+    return (
+        "header component"
+    );
+
+}
+
+export default Header;
+
+// assets
+// GlboalStyels msut be nclosed btu nto by errorBoudnary
