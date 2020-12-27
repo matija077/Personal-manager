041b0e175f1cccf074a73eeceda8e3f12652b34a
Matija077 <matija.prs@gmail.com>
2020-12-27 17:42:43 +0100

    add routes folder to backend

---
+++ src/routes/routes.route.ts
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var path = require('path');
+
+function router(app, params) {
+    console.log("sadasd" + path.resolve(__dirname));
+    var apiPath = path.resolve(__dirname, "api");
+    fs.readdirSync(apiPath).map(function(file) {
+        console.log(path.resolve(apiPath, file));
+            require(path.resolve(apiPath, file))(app, params);
+        }
+    )
+    console.log("route");
+}
+
+module.exports = router;
