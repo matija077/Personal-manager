cd7f65d40a4f38c25dbf4064de23eba64185c1a5
Matija077 <matija.prs@gmail.com>
2020-12-30 11:38:12 +0100

    add persitent storage

--- new_client/src/redux/utils.ts
+++ new_client/src/redux/utils.ts
@@ -1,4 +1,5 @@
 import { auth } from './utils.firebase';
+import React from 'react';
 
 function login(
     email: string,
@@ -9,6 +10,21 @@ function login(
     return auth.signInWithEmailAndPassword(email, password)
 }
 
+function usePersistedStorage(key: string, defaultValue: unknown): [unknown, Function] {
+    const [state, setState] = React.useState(() => {
+        const persistedState = localStorage.getItem(key);
+        return persistedState ? JSON.parse(persistedState) : defaultValue;
+    });
+
+    React.useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state))
+        console.log(state);
+    }, [state, key])
+
+    return [state, setState];
+}
+
 export {
     login,
+    usePersistedStorage
 };
