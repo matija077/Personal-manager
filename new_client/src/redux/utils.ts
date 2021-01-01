e7d0f8d8f8e04339e5c5ab37685473c97b2b2fb2
Matija077 <matija.prs@gmail.com>
2021-01-01 14:03:59 +0100

    add comment for usePersisted Storage function

--- new_client/src/redux/utils.ts
+++ new_client/src/redux/utils.ts
@@ -10,7 +10,16 @@ function login(
     return auth.signInWithEmailAndPassword(email, password)
 }
 /**
- *
+ * Custom Hook for using persisted storage in Function components
+ * it uses useState for data storage and useEffect for side effects
+ * useState - because useState intial value is computed on each re render
+ * (even tough value is disregarded) we want to cache the funcion doing the
+ * computation to run only on the first render. so we use argumetn less function
+ * useEffect - it runs on each key or state changed.
+ * so whenever we change the state, functio ncomponent will re render and then
+ * useEffect will run and save newest state to the perissted storage. useEffect will also run if we change the key
+ * but not the state to save onto persistedStorage with the newset key.
+ * TODO How to handle key deletion fro mstorage
  * @param key
  * @param defaultValue
  * @param callbackFunctionsArray
