b695a37d3a5e154d49906b81c0fab586c5660f5f
Matija077 <matija.prs@gmail.com>
2021-01-01 17:38:40 +0100

    login componnt

---
+++ new_client/src/components/login/login.container.tsx
@@ -0,0 +1,11 @@
+import Login from './login';
+
+function LoginContainer(props: any) {
+
+    return (
+        <Login />
+    );
+
+}
+
+export default LoginContainer;
---
+++ new_client/src/components/login/login.tsx
@@ -0,0 +1,11 @@
+import {
+    LoginStyles
+} from './login.styles';
+
+function Login() {
+    <LoginStyles>
+        Login Page
+    </LoginStyles>
+}
+
+export default Login;
