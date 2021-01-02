115f068c3b18008ea69fe2cf759b08af61395680
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-01-02 17:22:08 +0100

    add login type pickers

--- new_client/src/components/login/login.style.ts
+++ new_client/src/components/login/login.style.ts
@@ -1,7 +1,42 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-var LoginStyles = styled.div``;
+var LoginStyles = styled.main`
+    position: relative;
+    width: 30vw;
+    height: 50vh;
+    top: 25vh;
+    left: 35vw;
+    filter: contrast(300%);
+    background-color: blue;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+`;
+
+var loginPlaceholder = css`
+    filter: contrast(100%);
+    text-align: center;
+`;
+
+
+var LoginPickerLabelStyles = styled.label`
+    ${loginPlaceholder}
+    
+    
+`;
+
+var LoginPickerButton = styled.button`
+    ${loginPlaceholder}
+    width: 75%;
+    margin-top: 0.5rem;
+    background-color: yellow;
+`;
+
 
 export {
-    LoginStyles
+    LoginStyles,
+    LoginPickerLabelStyles,
+    LoginPickerButton
 };
--- new_client/src/components/login/login.tsx
+++ new_client/src/components/login/login.tsx
@@ -1,12 +1,26 @@
 import {
-    LoginStyles
+    LoginStyles,
+    LoginPickerLabelStyles,
+    LoginPickerButton
 } from './login.style';
 
 function Login(props: any) {
 
     return (
         <LoginStyles>
-            Login Page
+            <LoginPickerLabelStyles>
+                Choose login option:
+            </LoginPickerLabelStyles>
+            <LoginPickerButton
+                data-id="google"         
+            >
+                Google
+            </LoginPickerButton>  
+            <LoginPickerButton
+                data-id="standard"  
+            >
+                Email and Paswword
+            </LoginPickerButton>        
         </LoginStyles>
     );
 }
