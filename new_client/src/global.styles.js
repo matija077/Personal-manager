73f942022f1a41193ae392e3bab2c8a651538138
Matija077 <matija.prs@gmail.com>
2021-01-09 09:13:29 +0100

    fix stzles

--- new_client/src/global.styles.js
+++ new_client/src/global.styles.js
@@ -1,13 +1,9 @@
 import { createGlobalStyle  } from 'styled-components';
-import img from './assets/background.jpg';
+// import img from './assets/background.jpg';
 
 var GlobalStyle = createGlobalStyle`
     body{
         background-color: white;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-attachment: fixed;
         padding: 0;
         margin: 0;
     }
