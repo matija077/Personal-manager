b7223b94208c3502d3ef53a79680793dab1bc89f
Matija077 <matija.prs@gmail.com>
2021-01-10 13:20:50 +0100

    add home page container

---
+++ new_client/src/pages/home/homeContainer.page.tsx
@@ -0,0 +1,16 @@
+import  HomePage from './home.page';
+
+type HomeContainerPropsType = {
+    children: []
+};
+
+function HomePageContainer(props: HomeContainerPropsType) {
+
+    return (
+        <HomePage>
+
+        </HomePage>
+    );
+}
+
+export default HomePageContainer;
