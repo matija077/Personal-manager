833041b331bc05c24c5047ee9abdbf00d931ecb9
Matija077 <matija.prs@gmail.com>
2021-01-26 16:15:36 +0100

    add helmet, remove body parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+var helmet = require('helmet');
 var auth = require('./src/routes/api/auth.route.ts');
 
 var { resolvers, server: graphServer } = require('./src/graphql.ts');
@@ -8,7 +8,8 @@ var { resolvers, server: graphServer } = require('./src/graphql.ts');
 const PORT = 5010;
 const returnCodes = {
     'error': 500,
-    'ok': 200
+    'ok': 200,
+    'noContent': 204
 };
 
 if (process.env.NODE_ENV !== 'production') {
@@ -23,27 +24,37 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(_-__dirname, "client/build", "index.html"));
 })*/
 
+//midlewares
+app.use(helmet());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use('/api*', function cors(req, res, next) {
     console.log(req.get('origin'));
     const origin = req.get('origin');
-    const headers = {
-        'Access-Control-Allow-Origin' : `${origin}`,
-        'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
-    };
+
 
     if (req.method === "OPTIONS") {
-        res.writeHead(204, headers);
+        const headers = {
+            'Access-Control-Allow-Origin' : `${origin}`,
+            'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        };
+
+        res.writeHead(returnCodes.noContent, headers);
         res.end();
         return;
     }
 
     next();
 });
-app.use(bodyParser.json());
 
-app.post('/api/verifyToken', auth.verifyToken);
+
+//graphql middleware
 graphServer.applyMiddleware({app, path: "/graphql"})
+
+//routes
+app.post('/api/verifyToken', auth.verifyToken);
+
 
 //require(path.join(__dirname, "src", "routes", "routes.route.ts"))(app);
 
