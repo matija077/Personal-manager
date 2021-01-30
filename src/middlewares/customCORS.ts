8aaeede1dca1dc75beb585d296bb68a3d3160772
matija077 <matija.prs@gmail.com>
2021-01-30 16:36:51 +0100

    workable CORS headers for complex requests

--- src/middlewares/customCORS.ts
+++ src/middlewares/customCORS.ts
@@ -2,18 +2,21 @@ import { returnCodes } from "../config/utils";
 import express from 'express';
 
 function cors(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const origin = req.get('origin');
+
     if (req.method === "OPTIONS") {
-        console.log("olaaaa");
         const headers = {
             'Access-Control-Allow-Origin' : `${origin}`,
             'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
+            'Access-Control-Allow-Headers': 'Content-Type, Custom-Header'
         };
 
         res.writeHead(returnCodes.noContent, headers);
         res.end();
         return;
     }
+
+    res.header('Access-Control-Allow-Origin', origin || "");
 
     next();
 }
--- src/routes/api/auth.route.ts
+++ src/routes/api/auth.route.ts
@@ -3,10 +3,18 @@ const router = express.Router();
 import { authenticate } from '../../services/auth.service';
 
 router
-    .get("/authenticate", async (req: express.Request, res: express.Response) => {
+    .post("/authenticate", async (req: express.Request, res: express.Response) => {
+        console.log(req.params);
         const isAuthenticated = await authenticate({id: "1", password: "1234"});
 
         console.log(isAuthenticated);
+
+        const origin = req.get('origin');
+        const headers = {
+            'Access-Control-Allow-Origin' : `${origin}`
+        }
+
+        //origin && res.setHeader('Access-Control-Allow-Origin',  origin);
         res.send(isAuthenticated);
     })
 
