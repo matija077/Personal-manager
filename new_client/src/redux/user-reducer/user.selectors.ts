e5a0ff339eeb1509fa99839f49f953b818dd7313
Matija077 <matija.prs@gmail.com>
2021-02-04 22:43:44 +0100

    shamefullz deleting stupid code

--- new_client/src/redux/user-reducer/user.selectors.ts
+++ new_client/src/redux/user-reducer/user.selectors.ts
@@ -1,14 +1,6 @@
 import { RootState } from '../root-reducer';
 import { userStateType } from './user.types';
 
-var userMemo = {} as userStateType;
-
 export function getUser(state: RootState) {
-    //console.log("i have run");
-    if (state.user === userMemo) {
-        return userMemo;
-    }
-
-    userMemo = state.user;
     return state.user;
 }
