e7e660aa2e26a4e6b2d1f310513dc1e1e8bb038a
Matija077 <matija.prs@gmail.com>
2021-03-16 13:10:36 +0100

    refactoring auth route
    cookie creation.
    handle tokens in refreshToken route

--- src/routes/api/auth.route.ts
+++ src/routes/api/auth.route.ts
@@ -1,8 +1,25 @@
-import express, { NextFunction } from "express";
+import express, { CookieOptions, NextFunction } from "express";
 const router = express.Router();
+import handleToken from '../../middlewares/handleToken.middleware';
 import { authenticate } from '../../services/auth.service';
 import { createToken, createRefreshToken } from '../../services/token.service';
 
+const cookieOptions: CookieOptions = {
+    maxAge: 17280000000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+}
+function createAndSetCookie(name: string, token: string, options: CookieOptions = cookieOptions, res: express.Response) {
+    res.cookie(
+        name,
+        token,
+        options
+    )
+}
+function createAndSetRefreshTokenCookie( token: string, res: express.Response) {
+    createAndSetCookie("refreshToken", token, undefined, res);
+}
 
 router
     .post("/authenticate", async (req: express.Request, res: express.Response, next: NextFunction) => {
@@ -10,31 +27,26 @@ router
 
         try {
             const { isMatched: isAuthenticated, nickname } = await authenticate({email, password});
-            const token = nickname? await createToken(nickname) : undefined;
+            const { token = "", expiresIn = "" } = nickname ? await createToken(nickname) : {};
             const refreshToken = nickname? await createRefreshToken(nickname) : undefined;
 
-            res.cookie(
-                "refreshToken",
-                refreshToken,
-                {
-                    maxAge: 17280000000,
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: "none"
-                }
+            refreshToken && createAndSetRefreshTokenCookie(refreshToken, res);
 
-            )
-
-            res.json({isAuthenticated, nickname, token});
+            res.json({isAuthenticated, nickname, token, expiresIn});
 
         } catch (error: any) {
             next(error);
         }
     })
 
-    .post("/refreshToken", async (req: express.Request, res: express.Response, next: NextFunction) => {
+    .post("/refreshToken", handleToken,  async (req: express.Request, res: express.Response, next: NextFunction) => {
         res.json(req.cookies);
         console.log(req.headers.cookie);
+
+        // TODO -veryf token adn read data
+
+
+
     })
 
     .get("/verifyToken", async (req: express.Request, res: express.Response) => {
